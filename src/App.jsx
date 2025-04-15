67cdb7cec449ead69eb249afd1195506b160a63c
Gilad Yair <90310500+Gil100@users.noreply.github.com>
2025-04-15 15:26:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+iimport L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
+// הגדרת אייקונים מותאמים שייטענו מתוך public/icons
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow
+  iconRetinaUrl: "icons/marker-icon-2x.png",
+  iconUrl: "icons/marker-icon.png",
+  shadowUrl: "icons/marker-shadow.png"
 });
 
 
